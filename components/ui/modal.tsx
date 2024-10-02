b6a45d7222dd05c5ba23ac24cc129150353c6e35
FIvER4IK <andrshatalov121@gmail.com>
2024-10-02 20:51:15 +0300

    fix bug in wishlists page and wishlist del

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -10,8 +10,14 @@ type ModalProps = {
 
 export function Modal({ children, onClose }: ModalProps) {
   return createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-md shadow-lg max-w-md w-full">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      onClick={onClose} // Use onClose when the backdrop is clicked
+    >
+      <div
+        className="bg-white rounded-md shadow-lg max-w-md w-full"
+        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+      >
         {children}
       </div>
     </div>,
