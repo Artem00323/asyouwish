3ebd9ab332bba9ef8b81b8e62fd0be0d06a8f70a
Артем Захаров <artem.zakharov003@gmail.com>
2024-11-05 21:00:01 +0300

    yandex metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,27 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <script type="text/javascript">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98829853, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/98829853" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
