3fb184aeee221b7dd71dd33e7a123208c0092636
Artem00323 <artem.zakharov003@gmail.com>
2024-11-12 21:01:54 +0300

    fucking eslint again

--- components/quiz-page.tsx
+++ components/quiz-page.tsx
@@ -87,7 +87,7 @@ export default function QuizPage() {
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 />
                 <p className="text-muted-foreground text-xs text-center">
-                  We'll remind your friends about your special day
+                  We&apos;ll remind your friends about your special day
                 </p>
                 <Button 
                   className="w-3/4 mt-4 h-10 text-base" 
